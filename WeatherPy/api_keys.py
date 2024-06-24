ef7b917a6b1a0579e1ec6ad6088c5f7e210af448
FawneeCakes <fawn.payton@gmail.com>
2024-06-24 18:41:09 -0400

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "15d99dca10a8aafb3106eafb0590e5f1"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "302462bbb82544ddbfc2ea9dd561d5a0"
